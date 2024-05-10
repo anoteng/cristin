be6a4eb854172c212e46eb712fc5a529b5ba3ba0
anoteng <andreas@noteng.no>
2024-05-10 10:56:09 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap Venstremeny Mal</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <nav class="nav flex-column">
+          <a class="nav-link active" aria-current="page" href="#">Hjem</a>
+          <a class="nav-link" href="#">Om oss</a>
+          <a class="nav-link" href="#">Tjenester</a>
+          <a class="nav-link" href="#">Kontakt</a>
+        </nav>
+      </div>
+
+      <div class="col-md-9">
+        <h1>Hovedinnhold</h1>
+        <p>Dette er et eksempel på en Bootstrap venstremeny mal. Du kan tilpasse denne malen for å passe dine behov ved å legge til ditt eget innhold og stiler.</p>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
